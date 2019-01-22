e6fb5770edec077ba75d51e21304e52577e98ffa
Guillaume DONVAL <guillaume.donval@fxinnovation.com>
2019-01-22 16:50:01 -0500

    refactor: auto update of documentation & fmt

--- examples/user-with-credentials/main.tf
+++ examples/user-with-credentials/main.tf
@@ -7,6 +7,6 @@ provider "aws" {
 module "service_user_bootstrap" {
   source = "../../"
 
-  service_user_name = "${var.region}"
+  service_user_name               = "${var.region}"
   create_service_user_credentials = 1
 }
